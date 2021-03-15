ec500b5ecc4fa6fb476a7c53974c0f81f2640795
neyneyalldayday <drfrankenstein123@gmail.com>
2021-03-15 10:28:40 -0500

    fixed email functionality

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import styled from 'styled-components';
+import {Button} from '../components/Button'
 import{ init } from 'emailjs-com';
 init("user_mhbJdi6jtA3khrB3mb8ec");
 
@@ -21,13 +22,67 @@ background: #efe1fb;
 }
 `;
 
+const Form = styled.form`
+position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Subject = styled.input`
+display: block;
+padding-left: 10px;
+outline: none;
+border-radius: 2px;
+height: 40px;
+width: 60%;
+border: none;
+`;
+
+const Name = styled.input`
+display: block;
+padding-left: 10px;
+outline: none;
+border-radius: 2px;
+height: 40px;
+width: 60%;
+border: none;
+`;
+
+const EmailAddress = styled.input`
+display: block;
+padding-left: 10px;
+outline: none;
+border-radius: 2px;
+height: 40px;
+width: 60%;
+border: none;
+`;
+
+const Textarea = styled.textarea`
+display: block;
+padding-left: 10px;
+outline: none;
+border-radius: 2px;
+height: 40px;
+width: 60%;
+margin-bottom: 5px;
+border: none;
+`;
+
 
 const Email = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
     
-        emailjs.sendForm('gmail', 'service_btgzq6q', e.target, 'user_mhbJdi6jtA3khrB3mb8ec')
+        emailjs.sendForm('service_btgzq6q', 'template_elt7e33', e.target, 'user_mhbJdi6jtA3khrB3mb8ec')
           .then((result) => {
               console.log(result.text);
           }, (error) => {
@@ -39,16 +94,19 @@ const Email = () => {
     return (
         <Container>
             <Section>
-            <form  onSubmit={sendEmail}>
-                    <input type="text" placeholder="Subject" name="subject" />
+            <Form  onSubmit={sendEmail}>
+                    <Subject type="text" placeholder="Subject" name="subject" />
                         <label>Name</label>
-                            <input type="text" placeholder="Your name" name="name" />
+                            <Name type="text" placeholder="Your name" name="name" />
                             <label>Email</label>
-                            <input type="email" placeholder="Email Address" name="email" />
+                            <EmailAddress type="email" placeholder="Email Address" name="email" />
                        <label>Message</label>
-                    <textarea name="message" placeholder="Your message" />
-                    <input type="submit" value="Send Message" />
-                </form>
+                    <Textarea name="message" placeholder="Your message" />
+                    <Button>
+                    <input type="submit" value="Send Message" />  
+                    </Button>
+                    
+                </Form>
             </Section>                
         </Container>
        
