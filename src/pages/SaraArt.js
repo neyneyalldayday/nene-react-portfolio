3c9b136f7bf8f901fa31123f83d847954fe30a87
neyneyalldayday <drfrankenstein123@gmail.com>
2022-07-25 17:35:49 -0500

    deleted some redundant imports

--- src/pages/SaraArt.js
+++ src/pages/SaraArt.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import Art from '../components/Art'
 import {ArtData} from '../data/ArtData'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from 'react-router-dom'
+
 
 const SaraArt = () => {
   return (
